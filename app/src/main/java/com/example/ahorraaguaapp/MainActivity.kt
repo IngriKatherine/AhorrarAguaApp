a8facc805502db28166ab64eaa68aa7ca2d2d8e4
Juanito <59847854+IngriKatherine@users.noreply.github.com>
2024-09-29 17:32:38 -0400

    Edite como se veia en la previsualizacion porque estaba muy grande

--- app/src/main/java/com/example/ahorraaguaapp/MainActivity.kt
+++ app/src/main/java/com/example/ahorraaguaapp/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.ahorraaguaapp.databinding.ActivityMainBinding
 
@@ -36,22 +38,25 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun ActivityMainPreview() {
+fun ActivityMainPreview(modifier: Modifier = Modifier) {
     // Access the current context
     val context = LocalContext.current
 
     // Use AndroidView to preview activity_main.xml layout in Compose
-    AndroidView(factory = {
-        // Obtain the layout inflater from the context
-        val layoutInflater = android.view.LayoutInflater.from(context)
-        // Inflate your layout using the view binding
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.root
-    })
+    AndroidView(
+        factory = {
+            // Obtain the layout inflater from the context
+            val layoutInflater = android.view.LayoutInflater.from(context)
+            // Inflate your layout using the view binding
+            val binding = ActivityMainBinding.inflate(layoutInflater)
+            binding.root
+        },
+        modifier = modifier
+    )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 400, heightDp = 800) // Adjust the preview size here
 @Composable
 fun PreviewActivityMain() {
-    ActivityMainPreview()
+    ActivityMainPreview(modifier = Modifier)
 }
